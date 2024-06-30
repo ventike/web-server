4474201707faba95d2fad445548bf6aa9de43faa
Seyon Satheesh <seyon.satheesh@gmail.com>
2024-06-29 22:20:55 -0400

    Added IDS USers

--- api/serializers.py
+++ api/serializers.py
@@ -21,7 +21,7 @@ class UserAdminSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["username", "email", "first_name", "last_name", "role", "creation_date", "profile_picture"]
+        fields = ["pk", "username", "email", "first_name", "last_name", "role", "creation_date", "profile_picture"]
 
 ######################################################################################################
 
