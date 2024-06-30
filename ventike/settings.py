f459c38b459498652b683d91dd96e28e99d21082
Seyon Satheesh <seyon.satheesh@gmail.com>
2024-06-29 22:34:38 -0400

    Turned Debug Mode Off

--- ventike/settings.py
+++ ventike/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 # CSRF_TRUSTED_ORIGINS = ["http://*.on-acorn.io", "https://*.on-acorn.io"]
